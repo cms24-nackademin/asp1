395481b8d7fa5b198d9f7b6015914ce3e664e10a
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2025-03-14 09:08:42 +0100

    Reboot

---
+++ reboot/AlphaAdminPortal/WebApp/Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+
+
+[Route("admin")]
+public class AdminController : Controller
+{
+
+    [Route("clients")]
+    public IActionResult Clients()
+    {
+        return View();
+    }
+}
---
+++ reboot/AlphaAdminPortal/WebApp/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+app.UseHsts();
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Dashboard}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
