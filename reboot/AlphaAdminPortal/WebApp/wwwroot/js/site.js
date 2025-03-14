9501e7be15e6e68dd6821069f62e3565c9ae47ce
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2025-03-14 12:23:53 +0100

    Modal Added

--- reboot/AlphaAdminPortal/WebApp/wwwroot/js/site.js
+++ reboot/AlphaAdminPortal/WebApp/wwwroot/js/site.js
@@ -1,1 +1,46 @@
-﻿
+﻿document.addEventListener('DOMContentLoaded', () => {
+
+    initOpenModals()
+    initCloseButtons()
+})
+
+function initOpenModals() {
+    const modalButtons = document.querySelectorAll('[data-modal="true"]')
+    modalButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const target = button.getAttribute('data-target')
+            const modal = document.querySelector(target)
+
+            if (modal) {
+                modal.classList.add('flex')
+            }
+        })
+    })
+}
+
+function initCloseButtons() {
+    const closeButtons = document.querySelectorAll('[data-close="true"]')
+    closeButtons.forEach(button => {
+
+        button.addEventListener('click', () => {
+            const target = button.getAttribute('data-target')
+            const targetElement = document.querySelector(target)
+
+            if (targetElement) {
+                if (targetElement.classList.contains('modal')) {
+                    closeModal(targetElement)
+                }
+            }
+        })
+    })
+}
+
+function closeModal(modal) {
+    if (modal) {
+        modal.classList.remove('flex')
+
+        modal.querySelectorAll('form').forEach(form => {
+            form.reset()
+        })
+    }
+}
