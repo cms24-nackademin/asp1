c2d304034c9c634e1561398f71af254f24e3b3b3
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2025-03-21 09:31:15 +0100

    Authentication + Members

--- MemberManagement/WebApp/obj/Debug/net9.0/WebApp.AssemblyInfo.cs
+++ MemberManagement/WebApp/obj/Debug/net9.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e55c49ccd8e5a9c17dca628a25109743d646cd63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff9e2e79cd9c4b36cef09bd73464e6236f4e505c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
