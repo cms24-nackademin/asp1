00c7a43f0bf768a76d2c6bdd544e33009a04c740
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2025-03-28 12:33:56 +0100

    Lektion 8 (2025-03-28)

---
+++ lektion-8-2025-02-28/BackOffice/Domain/Extensions/MappingExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Extensions;
+
+public static class MappingExtensions
+{
+    public static TDestination MapTo<TDestination>(this object source)
+    {
+        
+    }
+}
--- lektion-8-2025-02-28/BackOffice/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ lektion-8-2025-02-28/BackOffice/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4c330e3cc4d03a0d4813d2eead2fbd2481f885f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0387f70a849abff204eb027ebae237e705bcfd22")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
